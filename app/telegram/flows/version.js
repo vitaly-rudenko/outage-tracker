0deecf2121f69cd58d18bc9d9347d4dcd10aacae
Vitaly Rudenko <maurice.dederick@gmail.com>
2022-12-20 03:11:08 +0200

    Better /version

--- app/telegram/flows/version.js
+++ app/telegram/flows/version.js
@@ -2,10 +2,21 @@ import fs from 'fs'
 import path from 'path'
 
 const packageJsonPath = path.join(process.cwd(), 'package.json')
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf-8' }))
+const packageJson = JSON.parse(
+  fs.readFileSync(packageJsonPath, { encoding: 'utf-8' })
+)
 
 export function versionCommand() {
   return async (context) => {
-    await context.reply(packageJson.version)
+    const { localize } = context.state
+
+    await context.reply(
+      localize('version', {
+        version: packageJson.version,
+        chatId: context.chat.id,
+        fromId: context.from.id,
+      }),
+      { parse_mode: 'MarkdownV2' }
+    )
   }
 }
